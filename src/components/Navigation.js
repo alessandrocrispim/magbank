214f3076f5e2a428bd2302493e85b285a9db0195
alessandrocrispim <alessandro.crispim.1979@gmail.com>
2023-04-12 18:24:09 -0300

    atualização

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,32 +1,51 @@
-import React from 'react'
+import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import "./Navigation.scss";
+import logo from "../assets/logo.svg"
 
 function Navigation() {
   return (
-    <Navbar id='teste'  expand="lg">
+    <Navbar variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            height="30"
+            className="d-inline-block align-top"
+            alt="MagBank logo"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#cartao">Cartão</Nav.Link>
+            <Nav.Link href="#quemsomos">Quem Somos</Nav.Link>
+            <Nav.Link href="#faq">FAQ</Nav.Link>
           </Nav>
+
+          <ButtonGroup aria-label="Basic example">
+            <Button variant="outline-light">
+              <NavDropdown title="Acessar minha conta" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Button>
+            <Button variant="outline-light">Abra sua conta</Button>
+          </ButtonGroup>
         </Navbar.Collapse>
       </Container>
     </Navbar>
