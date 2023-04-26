461df7a8ed53de5d79833b36a6ef811f532ce3ad
alessandrocrispim <alessandro.crispim.1979@gmail.com>
2023-04-26 09:27:17 -0300

    aula19

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,28 +1,32 @@
 import React from "react";
-import { Container, Row, Col, Button, Tabs, Tab, Table } from "react-bootstrap";
+import { Routes, Route, Link } from "react-router-dom";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle, faUser } from "@fortawesome/free-solid-svg-icons";
 
+import AccountBalance from "../components/AccountBalance";
+
 import "./Dashboard.scss";
 
-const Dashboard = ( {className = false}) => {
-  const data = [
-    { date: "22/07", description: "SAQUE 24H 012345", value: "300,00" },
-    { date: "21/07", description: "SUPERMERCADO 02323626", value: "275,00" },
-    { date: "20/07", description: "NETFLIX 235236", value: "30,00" },
-    { date: "22/07", description: "FARMACIA 121512", value: "350,00" },
-    { date: "22/07", description: "FARMACIA 121512", value: "350,00" },
-  ];
-
-  const future = [
-    { date: "22/08", description: "SALARIO  012345", value: "3000,00" },
-    { date: "21/08", description: "IMAGINE 02323626", value: "275,00" },
-    { date: "20/08", description: "NETFLIX 235236", value: "30,00" },
-    { date: "22/08", description: "FARMACIA 121512", value: "350,00" },
-  ];
+const Dashboard = ({ className = false }) => {
+  const data = {
+    latestBalance: [
+      { date: "22/07", description: "SAQUE 24H 012345", value: "300,00" },
+      { date: "21/07", description: "SUPERMERCADO 02323626", value: "275,00" },
+      { date: "20/07", description: "NETFLIX 235236", value: "30,00" },
+      { date: "22/07", description: "FARMACIA 121512", value: "350,00" },
+      { date: "22/07", description: "FARMACIA 121512", value: "350,00" },
+    ],
+    futureBalance: [
+      { date: "22/08", description: "SALARIO  012345", value: "3000,00" },
+      { date: "21/08", description: "IMAGINE 02323626", value: "275,00" },
+      { date: "20/08", description: "NETFLIX 235236", value: "30,00" },
+      { date: "22/08", description: "FARMACIA 121512", value: "350,00" },
+    ],
+  };
 
   return (
-    <Container className={`dashboard py-5 ${className ? className : '' }`}>
+    <Container className={`dashboard py-5 ${className ? className : ""}`}>
       <Row>
         <Col xs={12} lg={4}>
           <Row className="d-fex align-items-center mb-5">
@@ -43,86 +47,32 @@ const Dashboard = ( {className = false}) => {
             </Col>
           </Row>
           <Row className="d-flex justify-content-start">
-            <Button
-              className="dashboard__button dashboard__button--active"
-              variant="link"
-              size="lg"
-            >
-              Minha Conta
-            </Button>
-
-            <Button className="dashboard__button " variant="link" size="lg">
-              Pagamentos
-            </Button>
-
-            <Button className="dashboard__button " variant="link" size="lg">
-              Extrato
-            </Button>
+            <Link to="/dashboard">
+              <Button
+                className="dashboard__button dashboard__button--active"
+                variant="link"
+                size="lg"
+              >
+                Minha Conta
+              </Button>
+            </Link>
+            <Link to="/dashboard/payment">
+              <Button className="dashboard__button " variant="link" size="lg">
+                Pagamentos
+              </Button>
+            </Link>
+            <Link to="/dashboard/history">
+              <Button className="dashboard__button " variant="link" size="lg">
+                Extrato
+              </Button>
+            </Link>
           </Row>
         </Col>
-        <Col xs={12} lg={3} className="mt-lg-5 pt-lg-4 ps-5">
-          <h3 className="my-5 ">Conta corrente</h3>
-          <h6>
-            <small>
-              <strong>Saldo em conta corrente</strong>
-            </small>
-          </h6>
-          <h4 className="text-success mb-4">
-            <small>R$</small>3.500<small>,00</small>
-          </h4>
-          <h6>
-            <small>
-              <strong>Cheque especial</strong>
-            </small>
-          </h6>
-          <p className="mb-0">Limite disponivel</p>
-          <p className="mb-4">R$ 5.000,00</p>
-          <Button variant="secondary">Ver extrato</Button>
-        </Col>
-        <Col xs={12} lg={5} className="mt-lg-5 pt-lg-5">
-          <Tabs className=" mt-5 pt-lg-5" defaultActiveKey="latest">
-            <Tab eventKey="latest" title="Últimos Lançamentos">
-              <Table striped borderless>
-                <thead>
-                  <tr>
-                    <th>Data</th>
-                    <th>Descrição</th>
-                    <th>Valor</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {data.map(({ date, description, value }) => (
-                    <tr key={description}>
-                      <td>{date}</td>
-                      <td>{description}</td>
-                      <td>{value}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </Tab>
-            <Tab eventKey="future" title="Lançamentos Futuros">
-              <Table striped borderless>
-                <thead>
-                  <tr>
-                    <th>Data</th>
-                    <th>Descrição</th>
-                    <th>Valor</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {future.map(({ date, description, value }) => (
-                    <tr key={description}>
-                      <td>{date}</td>
-                      <td>{description}</td>
-                      <td>{value}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </Tab>
-          </Tabs>
-        </Col>
+        <Routes>
+          <Route path="/dashboard/history" element={<h2>extratos</h2>} />
+          <Route path="/dashboard/payments" element={<h2>pagamentos</h2>} />
+          <Route path="/dashboard" element={<AccountBalance data={data} />} />
+        </Routes>
       </Row>
     </Container>
   );
